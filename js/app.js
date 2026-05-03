eb31a4f1ba3f986899db425b7a9563246aa9e9e8
patpat <tomaspatrickdanielle@gmail.com>
2026-05-03 19:36:21 +0800

    remove dark overlay

--- js/app.js
+++ js/app.js
@@ -330,13 +330,11 @@ function setupEventListeners() {
   
   function openDiagramPanel() {
     if (diagramPanel) diagramPanel.classList.add('open');
-    if (diagramOverlay) diagramOverlay.classList.add('visible');
     if (diagramToggleBtn) diagramToggleBtn.classList.add('open');
   }
   
   function closeDiagramPanel() {
     if (diagramPanel) diagramPanel.classList.remove('open');
-    if (diagramOverlay) diagramOverlay.classList.remove('visible');
     if (diagramToggleBtn) diagramToggleBtn.classList.remove('open');
   }
   
@@ -353,10 +351,6 @@ function setupEventListeners() {
   
   if (btnClosePanel) {
     btnClosePanel.addEventListener('click', closeDiagramPanel);
-  }
-  
-  if (diagramOverlay) {
-    diagramOverlay.addEventListener('click', closeDiagramPanel);
   }
   
   // Close on Escape key
